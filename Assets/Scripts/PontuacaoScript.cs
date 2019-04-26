d55c73afa9dc0b54cdc1c864c5c238c17b5d7dcd
Lucas <lucasrchaves1@gmail.com>
2019-04-26 15:32:16 -0300

    [8, 9] Coletaveis adicionados e funcionando

---
+++ Assets/Scripts/PontuacaoScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PontuacaoScript : MonoBehaviour
+{
+    public Text pontosUI;
+    int pontos;
+    
+    
+    // Start is called before the first frame update
+    void Start(){
+        pontos = 0;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+    public void incrementaPontos(){
+        this.pontos = this.pontos + 1;
+//        pontosUI.text = pontos.ToString();
+        Debug.Log(pontos);
+    }
+}
